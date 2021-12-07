8664a477b87ec469c77cb2d84202b84d80640600
Ray Flores <ray.m.flores@gmail.com>
2021-12-07 05:11:28 +0000

    created urls_new.ejs to handle post request

--- express_server.js
+++ express_server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; 
+function generateRandomString() {
+  let string = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6);
+  console.log(string);
+  return string;
+}
 
 app.set('view engine', 'ejs');
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -17,6 +30,11 @@ app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 //NOT WORKING: shortURL undefined... 
 app.get('/urls/:shortURL', (req, res) => {
   //res.send(req.params) DO NOT INCLUDE
